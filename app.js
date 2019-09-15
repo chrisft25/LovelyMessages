46ea7d5073231d8adb44ef4dc3f26300fe5066ae
Christopher Fuentes <fuenteschris75@gmail.com>
2019-09-14 18:09:55 -0600

    Trying to do that shit with Node only

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const express= require('express');
-var app= express();
-
 require('dotenv').config();
 
 
 const twilioData = {
-    sid: process.env.TWILIO_SID,
-    token: process.env.TWILIO_TOKEN,
+    sid: String(process.env.TWILIO_SID),
+    token: String(process.env.TWILIO_TOKEN),
     phone: process.env.TWILIO_PHONE
 }
 
@@ -19,23 +16,25 @@ const twilio = require('twilio');
 
 const client = new twilio(twilioData.sid, twilioData.token);
 
+console.log(twilioData.sid)
 
-const port= process.env.HOST_PORT || 3000;
+client.messages
+  .create({
+     body: 'Just trying new things, boi',
+     from: twilioData.phone,
+     to: baby.phone
+   })
+  .then(message => {
+      console.log(message.sid);
+});
 
-app.get('/', function(req, res){
-//     client.messages
-//   .create({
-//      body: 'Just trying new things, boi',
-//      from: twilioData.phone
-//      to: baby.phone
-//    })
-//   .then(message => {
-//       console.log(message.sid)
-//       res.send('Hello World! Message is sent');
-// });
-    res.send('Hello world')
-})
+// const port= process.env.HOST_PORT || 3000;
 
-app.listen(port,function(){
-    console.log('Example app listening on port '+ port)
-})
+// app.get('/', function(req, res){
+
+//     res.send('Hello world')
+// })
+
+// app.listen(port,function(){
+//     console.log('Example app listening on port '+ port)
+// })
