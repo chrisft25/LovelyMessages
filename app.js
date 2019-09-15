325ec5128545c5ce567178b56001690a7d4e1b06
Christopher Fuentes <fuenteschris75@gmail.com>
2019-09-14 23:49:30 -0600

    Testing database

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 require('dotenv').config();
 
-const db = require('./config/db')
+const db = require('./config/db');
 
 const twilio = {
     sid: process.env.TWILIO_SID,
@@ -31,8 +31,55 @@ db.end();
 // client.messages.create({
 //     to: baby.phone,
 //     from: twilio.phone,
-//     body: 'Que pedo, soy Twilio-chan'
+//     body: 'Hola, me llamo Pixel!, soy un bot creado por Chris que te enviara mensajes al azar durante el dia recordandote lo genial que eres :D'
 // })
 // .then((message)=>{
 //     console.log(message.sid);
+// });
+
+// client.messages.create({
+//   to: baby.phone,
+//   from: twilio.phone,
+//   body: 'Tambien te contare chistes o cosas random de vez en cuando (no te asustes) para subirte el animo :3'
+// })
+// .then((message)=>{
+//   console.log(message.sid);
+// });
+
+// client.messages.create({
+//   to: baby.phone,
+//   from: twilio.phone,
+//   body: 'Sometimes, I will text you on English, because Chris knows you love this language'
+// })
+// .then((message)=>{
+//   console.log(message.sid);
+// });
+
+
+// client.messages.create({
+//   to: baby.phone,
+//   from: twilio.phone,
+//   body: 'Si ves algun mensaje raro, es culpa de Chris, el esta llenando mi base de datos :)'
+// })
+// .then((message)=>{
+//   console.log(message.sid);
+// });
+
+
+// client.messages.create({
+//   to: baby.phone,
+//   from: twilio.phone,
+//   body: 'Espero conocerte pronto, y que sea una bonita experiencia! :D'
+// })
+// .then((message)=>{
+//   console.log(message.sid);
+// });
+
+// client.messages.create({
+//   to: '+50377419727',
+//   from: twilio.phone,
+//   body: 'Espero conocerte pronto, y que sea una bonita experiencia! :D'
+// })
+// .then((message)=>{
+//   console.log(message.sid);
 // });
